bc5fa2c5aa2d33a2f54bcf202f371d4a64d3ec65
IngIanRpo <ingianrpo@gmail.com>
2023-01-19 10:16:00 -0500

    ham

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,10 +5,10 @@ const NavBar = ()=> {
         <>
             <nav>
                 <ul>
-                    <li><a href="#" onClick={alert("You are already in home page")}>Home</a></li>
-                    <li><a href="#" onClick={alert("im learning about routes please waite while i learn... While you wait to me you can eat an hamburguer :3 🍔")}>About Us</a></li>
-                    <li><a href="#" onClick={alert("im learning about routes please waite while i learn... While you wait to me you can eat an hamburguer :3 🍔")}>Pircings</a></li>
-                    <li><a href="#" onClick={alert("im learning about routes please waite while i learn... While you wait to me you can eat an hamburguer :3 🍔")}>Contact Us</a></li>
+                    <li><a>Home</a></li>
+                    <li><a onClick={alert("im learning about routes please waite while i learn... While you wait to me you can eat an hamburguer :3 🍔")}>About Us</a></li>
+                    <li><a>Pircings</a></li>
+                    <li><a>Contact Us</a></li>
                 </ul>
             </nav>
 
