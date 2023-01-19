dd25d1c00a6ca49dbb30359a231fcb5b947ffb90
IngIanRpo <ingianrpo@gmail.com>
2023-01-19 10:17:00 -0500

    g

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = ()=> {
             <nav>
                 <ul>
                     <li><a>Home</a></li>
-                    <li><a onClick={alert("im learning about routes please waite while i learn... While you wait to me you can eat an hamburguer :3 🍔")}>About Us</a></li>
+                    <li><a onClick={alert("im learning about routes please wait while i learn... While you wait to me you can eat an hamburguer :3 🍔")}>About Us</a></li>
                     <li><a>Pircings</a></li>
                     <li><a>Contact Us</a></li>
                 </ul>
